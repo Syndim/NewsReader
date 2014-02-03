53f5a2a1ddabd6b61e1ec05f7dc27e4bf654e66a
Syndim <im@syndim.org>
2014-02-03 19:45:21 +0800

    Change db to MySQL

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'newspaper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'data.db'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'db.conf'),
+        }
     }
 }
 
