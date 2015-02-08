ed6944a8743ee6b93782231f5a2ce0fc3762db9f
Syndim <im@syndim.org>
2015-02-08 14:18:57 +0800

    Change model

--- website/cnbeta/models.py
+++ website/cnbeta/models.py
@@ -5,13 +5,13 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=100)
     intro = models.CharField(max_length=3000)
-    content = models.CharField(max_length=50000)
+    content = models.TextField(max_length=50000)
     created_at = models.CharField(max_length=1000)
     origin_id = models.IntegerField(db_index=True)
     origin_site = models.CharField(max_length=100)
 
 class Comment(models.Model):
-    content = models.CharField(max_length=50000)
+    content = models.TextField(max_length=50000)
     updated_at = models.CharField(max_length=1000)
     origin_id = models.IntegerField(db_index=True)
     origin_site = models.CharField(max_length=100)
